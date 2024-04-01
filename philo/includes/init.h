dc2d8da5940fa720eb29e6ab117e502541edeea7
Arsene-Baitenov <arsenebaitenov@gmail.com>
2024-04-01 22:46:47 +0300

    Move managing logic to own threads from obsorver.

--- philo/includes/init.h
+++ philo/includes/init.h
@@ -15,7 +15,7 @@
 
 # include "philo.h"
 
-void	init_philos(t_engine *en, t_philo *philos, t_mutex *forks, char **argv);
+void	init_philos(t_engine *en, t_philo *philos, t_mutex *forks, char **argv, bool *finish);
 void	init_forks(t_engine *engine, t_mutex *forks, int count);
 void	init_engine(t_engine *engine, t_philo *philos, t_mutex *forks);
 
